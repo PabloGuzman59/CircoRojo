c9f1f4f401d48dc8815cee9c5e0ab28ace386d14
pabloguz59 <pg5984543@gmail.com>
2025-11-27 03:07:09 -0400

    spawns terminados

---
+++ Assets/02_Scripts/Minion/MapMinionSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MapMinionSpawner : MonoBehaviour
+{
+    public GameObject minionPrefab;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 10f;
+    public int maxMinions = 6;
+
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            timer = 0;
+            TrySpawn();
+        }
+    }
+
+    void TrySpawn()
+    {
+        if (FindObjectsOfType<MinionAI>().Length >= maxMinions)
+            return;
+
+        // Elegir un punto aleatorio del mapa
+        Transform sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Instantiate(minionPrefab, sp.position, Quaternion.identity);
+        Debug.Log("MAP SPAWNER → Minion creado en punto fijo");
+    }
+}
---
+++ Assets/02_Scripts/Minion/PlayerMinionSpawner.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMinionSpawner : MonoBehaviour
+{
+    public GameObject minionPrefab;
+    public Transform player;
+
+    public float spawnInterval = 12f;
+    public int maxMinions = 4;
+
+    public float spawnRadius = 8f;
+    public float minDistance = 4f;
+
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            timer = 0;
+            TrySpawnNearPlayer();
+        }
+    }
+
+    void TrySpawnNearPlayer()
+    {
+        if (FindObjectsOfType<MinionAI>().Length >= maxMinions)
+            return;
+
+        Vector3 randomPos = Vector3.zero;
+        bool found = false;
+
+        for (int i = 0; i < 30; i++)
+        {
+            Vector2 circle = Random.insideUnitCircle * spawnRadius;
+
+            Vector3 potential = new Vector3(
+                player.position.x + circle.x,
+                player.position.y,
+                player.position.z + circle.y
+            );
+
+            // evitar spawn demasiado cerca
+            if (Vector3.Distance(player.position, potential) < minDistance)
+                continue;
+
+            // comprobar si está sobre navmesh
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(potential, out hit, 1.5f, NavMesh.AllAreas))
+            {
+                randomPos = hit.position;
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            Instantiate(minionPrefab, randomPos, Quaternion.identity);
+            Debug.Log("PLAYER SPAWNER → Minion creado cerca del jugador");
+        }
+        else
+        {
+            Debug.Log("PLAYER SPAWNER → No se encontró un punto válido.");
+        }
+    }
+}
